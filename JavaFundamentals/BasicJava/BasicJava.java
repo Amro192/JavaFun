13fb476e9be3a391a336d569d9c1ad7dd81d6ed2
Amro192 <amro2003.tariq@gmail.com>
2021-06-09 14:03:23 +0300

    Java

--- JavaFundamentals/BasicJava/BasicJava.java
+++ JavaFundamentals/BasicJava/BasicJava.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 public class BasicJava {
-    // public Integer TowHundred(){
+    // public void TowHundred(){
     //     int i =0 ;
     //     while (i<255){
     //         System.out.print(i);
     //         i++;
     //     }
-    //     return i;}
-    public Integer TowHundredArray(){
+    //    }
+    public void TowHundredArray(){
         int i = 0;
         ArrayList<Integer> myList = new ArrayList<Integer>();
         while(i<255){
@@ -16,8 +16,6 @@ public class BasicJava {
         }
         for (int x = 0; x < myList.size(); x++){
         System.out.println(myList.get(x));}
-        int y =0;
-        return y;
     }
 
 }
