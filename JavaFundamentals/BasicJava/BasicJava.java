4d485e0a60392e7a8369988bc066e481cd3e7562
Amro192 <amro2003.tariq@gmail.com>
2021-06-10 13:56:43 +0300

    basics

--- JavaFundamentals/BasicJava/BasicJava.java
+++ JavaFundamentals/BasicJava/BasicJava.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 public class BasicJava {
     // public void TowHundred(){
     //     int i =0 ;
@@ -7,15 +8,108 @@ public class BasicJava {
     //         i++;
     //     }
     //    }
-    public void TowHundredArray(){
-        int i = 0;
-        ArrayList<Integer> myList = new ArrayList<Integer>();
-        while(i<255){
-            i = i+i;
-            myList.add(i);
-        }
-        for (int x = 0; x < myList.size(); x++){
-        System.out.println(myList.get(x));}
-    }
 
-}
+
+    //     public void TowHundredOdd(){
+    //     int i =0 ;
+    //     while (i<=255){
+    //         if(i%2 != 0 ){
+    //         System.out.print(i);}
+    //         i++;
+    //     }
+    // }
+
+
+    // public void TowHundredArray(){
+    //     int sum =0;
+    //     for(int i=0; i<=255; i++){
+    //         sum = sum+i;
+    //         System.out.println(i +":"+ sum);
+    //     }
+    // }}
+
+
+    // public static void arrayPrint(){
+    //     int arr[] = {1,4,6,8,3};
+    //     for(int i =0; i<arr.length; i++){
+    //         System.out.println(arr[i]);
+    //     }
+    // }}
+
+
+    // public static void maxArray(){
+    //     int arr[] = {1,4,6,8,3,0,-4,-2,9};
+    //     int max = 0;
+    //     for(int i =0; i<arr.length; i++){
+    //         if(arr[i]>max){
+    //             max = arr[i];
+    //         }
+    //     }
+    //     System.out.print(max);
+    // }}
+
+
+    // public static void avgArray(){
+    //     int avg=0;
+    //     int sum=0;
+    //     int arr[] ={10,2,3};
+    //     for(int i =0; i<arr.length; i++){
+    //         sum = sum+arr[i];
+    //     }
+    //     avg =(sum/arr.length);
+    //     System.out.print(avg);
+    // }}
+
+
+    // public static ArrayList<Integer> OddArray() {
+	// 	ArrayList<Integer> arr = new ArrayList<Integer>();
+	// 	for(int i = 0; i <= 255; i++) {
+	// 		if(i % 2 != 0)
+	// 			arr.add(i);}
+    //             System.out.print(arr);
+	// 	return arr;
+	// }}
+
+
+    // public static Integer GreaterThan(int[] grater , int num){
+    //     int sum = 0;
+    //     for (int k : grater){
+    //         if(k>num){
+    //             sum++;
+    //         }
+    //     }
+    //     System.out.print(sum);
+    //     return sum;
+    // }}
+
+
+    // public static void SquareArray(int[] nums) {
+	// 	for(int i = 0; i < nums.length; i++) {
+	// 		nums[i] = nums[i] * nums[i];	
+	// 	}
+    //     System.out.println(Arrays.toString(nums));  
+	// }}
+
+
+    // public static void noNegativArray(int[] arr){
+    //     for(int i=0; i<arr.length; i++){
+    //         if(arr[i]<0){
+    //             arr[i] = 0;
+    //         }
+    //     }
+    //     System.out.print(Arrays.toString(arr));
+    // }}
+
+
+    // I did it before
+
+
+    // public static void shiftArray(int[] ar){
+    //     ArrayList<Integer> ar2 = new ArrayList<Integer>();
+    //     for(int i=1; i<ar.length; i++){
+    //         ar2.add(ar[i]);
+    //     }
+    //     // int last = ar[ar.length-1] =0;
+    //     ar2.add(0);
+    //     System.out.print(ar2);
+    // }}
--- JavaFundamentals/BasicJava/BasicJavamain.java
+++ JavaFundamentals/BasicJava/BasicJavamain.java
@@ -1,8 +1,8 @@
 public class BasicJavamain{
     public static void main(String[] args){
         BasicJava iD = new BasicJava();
-        int num = iD.TowHundredArray();
-        System.out.println(num);
+        int[] a = {1,2,-3,4,5};
+        iD.shiftArray(a);
     }
 
 }
